b592d9a5f0c25390a6f869173e1cfa43222fa64a
SaudaminiKhare <saudaminikhare@gmail.com>
2019-09-06 11:24:47 +0530

    Adding temp code

---
+++ StudentServices/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using StudentDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentServices.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public IEnumerable<student> Get()
+        {
+            using (StudentDetailsEntities entities=new StudentDetailsEntities())
+            {
+                return entities.student.ToList();
+            }
+        }
+        public student Get(int id)
+        {
+            using(StudentDetailsEntities entities=new StudentDetailsEntities())
+            {
+                return entities.student.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
